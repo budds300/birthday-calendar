a7b3a592bb7ceebceeb61e1bb5c8d66cd084d666
budds300 <budds300@gmail.com>
2021-04-10 15:52:23 +0300

    more declared variables

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ form.addEventListener('submit',(e)=>{
     e.preventDefault();
     const birthDate= document.querySelector("#birth-date").value;
     const gender= document.querySelector("#gender").value;
-   
+    if (gender && birthDate){
+        let spitValues = birthDate.split('-');
+        const year= parseInt(spitValues[0]);
+        const month= parseInt(spitValues[1]);
+        const day= parseInt(spitValues[2]);
+        let a = Math.floor((14-month)/12);
+       
