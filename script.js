80c4e049faef241cf68ea90857ae75c524a08aba
budds300 <budds300@gmail.com>
2021-04-10 15:36:28 +0300

    declare variables

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const form = document.querySelector(".addform");
-
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const birthDate= document.querySelector("#birth-date").value;
+    const gender= document.querySelector("#gender").value;
+   
